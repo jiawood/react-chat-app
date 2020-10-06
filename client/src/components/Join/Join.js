32d82a25168b9e697da751f7ceecbd1c64aa87a5
xiaojaja <17806251528@163.com>
2020-10-06 11:15:56 +0800

    shit

---
+++ client/src/components/Join/Join.js
@@ -0,0 +1,15 @@
+import React,{useState} from 'react'
+import {Link} from 'react-router-dom'
+
+import './Join.css'
+
+const Join = () => {
+
+  return (
+    <h1>
+      Join
+    </h1>
+  )
+}
+
+export default Join
